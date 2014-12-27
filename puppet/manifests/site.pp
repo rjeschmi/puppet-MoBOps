0e6db079fd34261358e34ffa23de4c0393ce5c6c
Robert Schmidt <rjeschmi@gmail.com>
2014-12-27 02:03:13 +0000

    move to github repo for mailman

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,3 +3,11 @@ class {'::postfix::server':
 	myhostname => 'keg.barleyment.ca',
 	mydomain   => 'barleyment.ca',
 }
+
+class {'::mailman':
+    default_url_host => 'lists.barleyment.ca',
+    default_email_host => 'lists.barleyment.ca',
+    mm_cfg_settings     => {
+        'MTA' => "'Postfix'",
+    }
+}
